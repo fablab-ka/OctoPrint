75d93878f00cfcfbf0a8737c1fa905cfd0bba6a4
Wolfgang Kraft <fablab-ka@wkraft.org>
2019-08-31 14:06:46 +0200

    solved conflicts in comm.py caused by merge with our serial port restriction

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -156,27 +156,33 @@ regex_resend_linenumber = re.compile("(N|N:)?(?P<n>%s)" % regex_int_pattern)
 
 def serialList():
 	baselist=[]
-	if os.name=="nt":
-		try:
-			key=_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"HARDWARE\\DEVICEMAP\\SERIALCOMM")
-			i=0
-			while(1):
-				baselist+=[_winreg.EnumValue(key,i)[1]]
-				i+=1
-		except:
-			pass
-	baselist = baselist \
-			   + glob.glob("/dev/ttyUSB*") \
-			   + glob.glob("/dev/ttyACM*") \
-			   + glob.glob("/dev/tty.usb*") \
-			   + glob.glob("/dev/cu.*") \
-			   + glob.glob("/dev/cuaU*") \
-			   + glob.glob("/dev/rfcomm*")
+	
+	if not settings().getBoolean(["serial", "useOnlyAdditionalPorts"]):
+		if os.name=="nt":
+			try:
+				key=_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"HARDWARE\\DEVICEMAP\\SERIALCOMM")
+				i=0
+				while(1):
+					baselist+=[_winreg.EnumValue(key,i)[1]]
+					i+=1
+			except:
+				pass
+		else:
+			baselist = baselist \
+						+ glob.glob("/dev/ttyUSB*") \
+						+ glob.glob("/dev/ttyACM*") \
+						+ glob.glob("/dev/tty.usb*") \
+						+ glob.glob("/dev/cu.*") \
+						+ glob.glob("/dev/cuaU*") \
+						+ glob.glob("/dev/rfcomm*")
 
 	additionalPorts = settings().get(["serial", "additionalPorts"])
 	if additionalPorts:
 		for additional in additionalPorts:
 			baselist += glob.glob(additional)
+	  baselist = list(set(baselist))	# remove possible duplicates through globs
+	  if not settings().getBoolean(["serial", "useOnlyAdditionalPorts"]):
+		  baselist.insert(0,"AUTO") # instead of inserting it in template
 
 	prev = settings().get(["serial", "port"])
 	if prev in baselist:
